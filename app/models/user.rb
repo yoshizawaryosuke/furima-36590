48c227501346131f413edb28788eea45efd65eda
yoshizawaryosuke <88420159+yoshizawaryosuke@users.noreply.github.com>
2021-09-02 15:33:00 +0900

    ビューファイル置き換え

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :name,               presence: true
+  validates :email,              presence: true
+  validates :encrypted_password, presence: true
+  validates :profile,            presence: true
+  validates :occupation,         presence: true
+  validates :position,           presence: true
 end
