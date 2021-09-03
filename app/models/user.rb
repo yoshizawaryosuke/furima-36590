bd52cfca5b207cff3bf918d8f11c9f3d0d40af4c
yoshizawaryosuke <88420159+yoshizawaryosuke@users.noreply.github.com>
2021-09-03 16:05:15 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+devise :database_authenticatable, :registerable,
+       :recoverable, :rememberable, :validatable
 
-  validates :nickname,           presence: true
-  validates :email,              presence: true
-  validates :encrypted_password, presence: true
-  validates :family_name,        presence: true
-  validates :first_name,         presence: true
-  validates :family_name_kana,   presence: true
-  validates :first_name_kana,    presence: true
-  validates :birthday,           presence: true
+validates :nickname, presence: true
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX}
-  
+with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+validates :family_name
+validates :first_name
+end
+
+with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+validates :family_name_kana
+validates :first_name_kana
+end
+
+VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates :password, format: { with: VALID_PASSWORD_REGEX}
+
+validates :birthday, presence: true
 end
