975ef78bf458c756f75c555b9e8e5d46a8205b79
yoshizawaryosuke <88420159+yoshizawaryosuke@users.noreply.github.com>
2021-09-04 15:30:53 +0900

    モデル、テーブルの作成とActiveStorageを導入

--- Gemfile
+++ Gemfile
@@ -64,3 +64,6 @@ gem 'devise'
 gem 'pry-rails'
 
 gem 'faker'
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,10 @@ class ItemsController < ApplicationController
   def index
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :image, :item_text, :category_id, :postage_id, :condition_id, :prefeture_id, :days_id, :price).merge(user_id: current_user.id)
+  end
+
 end
