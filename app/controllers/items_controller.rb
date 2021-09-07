f22985818d7cc625c3f669c90ae4437be515c4ca
yoshizawaryosuke <88420159+yoshizawaryosuke@users.noreply.github.com>
2021-09-07 18:57:54 +0900

    修正②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, except: [:index, :new, :create]
-  before_action :move_to_index, only: [:edit :destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def create
     @item = Item.new(item_params)
