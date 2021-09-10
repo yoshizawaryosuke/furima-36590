70eefab1c1e32745d28e57e598e52124e97f1d9e
yoshizawaryosuke <88420159+yoshizawaryosuke@users.noreply.github.com>
2021-09-10 16:39:17 +0900

    クレジット機能とテストコード

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -6,12 +6,29 @@ class BuysController < ApplicationController
 
   def create
     @buy_shipping_address= BuyShippingAddress.new(buy_params)
-    @buy_shipping_address.save
+    @item = Item.find(params[:item_id])
+    if @buy_shipping_address.valid?
+       pay_item
+       @buy_shipping_address.save
+       redirect_to root_path
+    else
+       render :index
+    end
   end
 
   private
 
   def buy_params
-    params.require(:buy_shipping_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
+    params.require(:buy_shipping_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :price).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: buy_params[:token],
+      currency: 'jpy'   
+    )
+  end
+
 end
---
+++ spec/factories/buy_shipping_addresses.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :buy_shipping_address do
+    postal_code   { '123-4567' }
+    prefecture_id { 1 }
+    city          { '埼玉県' }
+    house_number  { '1-1' }
+    building      { '埼玉ハイツ' }
+    phone_number  { '09012345678' }
+    association :item
+
+    token         {"tok_abcdefghijk00000000000000000"}
+  end
+end
